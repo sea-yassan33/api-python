39a53c453d388a5b055364fab6f10355f7d758bb
sea-yassan33 <a.yasuda_tr51@me.com>
2024-07-07 10:06:24 +0900

    update240707

--- flask_api_react2/js/app.js
+++ flask_api_react2/js/app.js
@@ -4,7 +4,13 @@ function App() {
     // スタイルオブジェクトの定義
     const styles = {
         sMadeBtn: 'btn btn-custom flex-fill',
-
+        prSkipContent: 'sr-only sr-only-focusable',
+        prNaveBar: 'navbar  fixed-top flex-md-nowrap p-0 shadow custom-bg',
+        prNaveBarContent: 'navbar-brand col-sm-3 col-md-2 mr-0',
+        prNaveBarContent02: 'col d-flex align-items-center justify-content-center',
+        prMainDisplay: 'px-4 min-vh-100',
+        prMainOption: 'd-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom',
+        prBtn01:'btn btn-sm btn-outline-secondary',
     };
 
     // 状態管理
@@ -57,7 +63,9 @@ function App() {
             case 'Api':
                 return (
                 <div>
-                    <h1>Data Annual expenditure on ice cream</h1>
+                    <div className={`${styles.prMainOption}`}>
+                        <h2 class="h2">Data Annual expenditure on ice cream</h2>
+                    </div>
                     <div class="canvas-container">
                         <canvas ref={chartRef} ></canvas>
                     </div>
@@ -65,11 +73,19 @@ function App() {
             );
             case 'Contact':
                 return (
-                <div>準備中です。上記メニューから再度選択してください。</div>
+                <div>
+                    <div className={`${styles.prMainOption}`}>
+                        <h2 class="h2">準備中です。上記メニューから再度選択してください。</h2>
+                    </div>
+                </div>
             );
             default:
                 return (
-                <div>上記メニューから選択して下さい。</div>
+                <div>
+                    <div className={`${styles.prMainOption}`}>
+                        <h2 class="h2">上記メニューから選択して下さい。</h2>
+                    </div>
+                </div>
             );
         }
     };
@@ -77,20 +93,43 @@ function App() {
     // コンポーネントのレンダリング
     return (
         <div>
-            <div className="navbar navbar-expand navbar-dark custom-bg">
-                <div className="container-fluid">
-                    <p className="navbar-brand text-black">Data</p>
-                    <div className="navbar-collapse">
-                        <ul className="navbar-nav">
-                            <button className={`${styles.sMadeBtn} `} onClick={() => setSelectedMenu('Api')}>アイスクリーム年間消費</button>
-                            <a className={`${styles.sMadeBtn}`} href="./japanMapPage.html">日本人口統計</a>
-                            <button className={`${styles.sMadeBtn} `} onClick={() => setSelectedMenu('Contact')}>Preparation</button>
-                        </ul>
+            <a id="skippy" className={`${styles.prSkipContent} `} href="#content">
+                <div className="container">
+                    <span className="skiplink-text">Skip to main content</span>
+                    
+                </div>
+            </a>
+            <nav className={`${styles.prNaveBar} `}>
+                <div className='row'>
+                    <div className={`${styles.prNaveBarContent02}`}>
+                        <p className={`${styles.prNaveBarContent}`} >Open Data</p>
+                    </div>
+                    <div className={`${styles.prNaveBarContent02}`}>
+                        <button className={`${styles.sMadeBtn}`} onClick={() => setSelectedMenu('Api')}>アイスクリーム年間消費</button>
+                    </div>
+                    <div className={`${styles.prNaveBarContent02}`}>
+                        <a className={`${styles.sMadeBtn}`} href="./japanMapPage.html">日本人口統計</a>
+                    </div>
+                    <div className={`${styles.prNaveBarContent02}`}>
+                        <button className={`${styles.sMadeBtn}`} onClick={() => setSelectedMenu('Contact')}>Preparation</button>
                     </div>
                 </div>
-            </div>
-            <div className="">
-                {renderContent()}
+                <ul className="navbar-nav px-3">
+                    <li className="nav-item text-nowrap">
+                        <div className="nav-link">React Page</div>
+                    </li>
+                </ul>
+            </nav>
+            <div className="mt-5">
+                <div className={`${styles.prMainDisplay}`}>
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <div className="btn-group mr-2">
+                            <button type="button" className={`${styles.prBtn01}`}>Share</button>
+                            <button type="button" className={`${styles.prBtn01}`}>Export</button>
+                        </div>
+					</div>
+                    {renderContent()}
+                </div>
             </div>
         </div>
     );
